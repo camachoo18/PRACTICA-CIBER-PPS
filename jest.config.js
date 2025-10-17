c684c44df89e3ab5b7d044008c3b25032b4a5d88
camachoo18 <estivencamachops@gmail.com>
2025-10-17 18:15:54 +0200

    añado informe de cobertura con nyc/istanbul

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,30 @@
 module.exports = {
   testEnvironment: 'node',
   coverageDirectory: 'coverage',
+  coverageReporters: ['html', 'text', 'text-summary'],
   collectCoverageFrom: [
-    'src/**/*.js',
-    'public/js/**/*.js',
-    '!src/server.js'
+    'tests/calculator-wrapper.js',
+    'src/routes/*.js',
+    '!src/server.js',
+    '!**/node_modules/**',
+    '!**/coverage/**'
   ],
   testMatch: [
     '**/tests/**/*.test.js',
     '**/tests/**/*.tests.js'
   ],
-  verbose: true
+  coveragePathIgnorePatterns: [
+    '/node_modules/',
+    '/coverage/',
+    'src/server.js'
+  ],
+  verbose: true,
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80
+    }
+  }
 };
